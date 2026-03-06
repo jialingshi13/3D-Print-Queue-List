47b36f64fd97b6207a9a282ced64914c2a8bdfc1
jialingshi13 <jialingshi90@gmail.com>
2026-03-06 15:38:57 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,6 @@ self.addEventListener('install', (e) => {
   self.skipWaiting();
   e.waitUntil(caches.open('q-v1').then(c => c.addAll([
     './', './index.html', './manifest.webmanifest',
-    './icon-192.png', './icon-512.png'
   ])));
 });
 self.addEventListener('activate', (e) => {
@@ -13,3 +12,4 @@ self.addEventListener('fetch', (e) => {
     caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
+
